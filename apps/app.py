8b0f5f109ca0f011ff8833dafa4ec91b1261c146
tomizi <106354750+tomizi@users.noreply.github.com>
2022-05-27 23:16:33 +0200

    Update app.py

--- apps/app.py
+++ apps/app.py
@@ -11,7 +11,7 @@ import streamlit as st
 import numpy as np
 from pathlib import Path
 
-os_path = Path(__file__).patern[0]
+os_path = Path(__file__).parents[0]
 st.set_page_config(page_title='Sales Dashboard', page_icon = ':bar_chart:',
                   layout='centered')
 df = pd.read_excel(open(os_path,'rb'))
