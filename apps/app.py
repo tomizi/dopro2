549bb2a62ea2d19b691bc831c7626fd43135fbd0
tomizi <106354750+tomizi@users.noreply.github.com>
2022-05-27 23:30:35 +0200

    Update app.py

--- apps/app.py
+++ apps/app.py
@@ -14,7 +14,9 @@ from pathlib import Path
 os_path = Path(__file__).parents[0] / 'DaneWydatki.xlsx'
 st.set_page_config(page_title='Sales Dashboard', page_icon = ':bar_chart:',
                   layout='centered')
-df = pd.read_csv(open(os_path,'rb'))
+with open(os_path, 'rb', encoding='utf8') as f:
+        df = pd.read_csv(f)
+
 
 
 st.sidebar.header('Please Filter Here:')
