904c67c16d91489791fc182ab1ca6b3160dc1fe1
tomizi <106354750+tomizi@users.noreply.github.com>
2022-05-27 23:31:01 +0200

    Update app.py

--- apps/app.py
+++ apps/app.py
@@ -14,7 +14,7 @@ from pathlib import Path
 os_path = Path(__file__).parents[0] / 'DaneWydatki.xlsx'
 st.set_page_config(page_title='Sales Dashboard', page_icon = ':bar_chart:',
                   layout='centered')
-with open(os_path, 'rb', encoding='utf8') as f:
+with open(os_path, 'r', encoding='utf8') as f:
         df = pd.read_csv(f)
 
 
