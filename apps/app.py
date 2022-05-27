47dea6eed079097d1f77868757822c5212fc6755
tomizi <106354750+tomizi@users.noreply.github.com>
2022-05-28 00:07:08 +0200

    Update app.py

--- apps/app.py
+++ apps/app.py
@@ -11,7 +11,7 @@ import streamlit as st
 import numpy as np
 from pathlib import Path
 
-os_path = Path(__file__).parents[0] / 'DaneWydatki.txt'
+os_path = Path(__file__).parents[0] / 'DaneWydatki.csv'
 st.set_page_config(page_title='Sales Dashboard', page_icon = ':bar_chart:',
                   layout='centered')
 df = pd.read_csv(open(os_path,'rb'),sep=';',header = 0)
