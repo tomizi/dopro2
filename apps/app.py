0f929d738c5523fcb964edac28c2486aaab01360
tomizi <106354750+tomizi@users.noreply.github.com>
2022-05-27 22:44:48 +0200

    Update app.py

--- apps/app.py
+++ apps/app.py
@@ -12,7 +12,7 @@ import numpy as np
 
 st.set_page_config(page_title='Sales Dashboard', page_icon = ':bar_chart:',
                   layout='centered')
-df = pd.read_csv('DaneWydatki')
+df = pd.read_csv('DaneWydatki.xlsx')
 
 
 st.sidebar.header('Please Filter Here:')
